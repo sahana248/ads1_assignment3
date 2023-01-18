86cfab4eec67faf34d432cc45b1c4344fe964693
sahana248 <117547972+sahana248@users.noreply.github.com>
2023-01-18 20:47:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 import seaborn as sns
 import sklearn.cluster as cluster
 import sklearn.metrics as skmet
+import itertools as itr
 
 def tablefunc(filename):
     """
@@ -155,6 +156,32 @@ def clustering(x,y):
     plt.title("cluster")
     plt.savefig('cluster'+ x + y)
     plt.show()
+
+def func(x,a,b,c):
+    """function to calculate the error limits"""
+    return a * np.exp(-(x-b)**2 / c)
+
+def err_ranges(x, func, param, sigma):
+    """
+    Calculates the upper and lower limits for the function, parameters and
+    sigmas for single value or array x. Functions values are calculated for 
+    all combinations of +/- sigma and the minimum and maximum is determined.
+    Can be used for all number of parameters and sigmas >=1.
+    """
+    # initiate arrays for lower and upper limits
+    lower = func(x, *param)
+    upper = lower
+    uplow = []   # list to hold upper and lower limits for parameters
+    for p,s in zip(param, sigma):
+        pmin = p - s
+        pmax = p + s
+        uplow.append((pmin, pmax))  
+    pmix = list(itr.product(*uplow))
+    for p in pmix:
+        y = func(x, *p)
+        lower = np.minimum(lower, y)
+        upper = np.maximum(upper, y) 
+    return lower, upper
 
 filenames= ['co2_per_capita','gdp','renew_energy_percent',
             'under5_mortality','unemployment','urban_population',
